3197a7c58e1e3e10fb6d4b2cb642f52d92c3d465
Marian Guceanu <george.guceanu@stud.ubbcluj.ro>
2022-07-20 13:06:55 +0300

    str form done correctly

--- Board/board.py
+++ Board/board.py
@@ -1,6 +1,3 @@
-from turtle import pos
-
-
 class Board:
     """
         The board for our game
@@ -14,32 +11,28 @@ class Board:
         for i in range(6):
             self.__bd.append(list())
             for j in range(7):
-                self.__bd[i].append("-  ")
+                self.__bd[i].append("- ")
 
     def getPiece(self, position: tuple):
-        return self.__bd[position[0], position[1]]
+        return self.__bd[position[0]][position[1]]
 
     def setPiece(self, position: tuple, player: int):
         if player == 0:
-            self.__bd[position[0] - 1][position[1] - 1] = "❤  "
+            self.__bd[position[0] - 1][position[1] - 1] = "A "
             return
-        self.__bd[position[0] - 1][position[1] - 1] = "💙  "
+        self.__bd[position[0] - 1][position[1] - 1] = "B "
 
     def __str__(self) -> str:
         """
-            Transforming the board into a str format
-        Returns:
-            str: String form of the board, with row and col indicators
+            Turning our board into a string
         """
-        final_string = ""
+        final_str = ""
         for i in range(6):
-            if i == 0:
-                final_string += "   "
-                for k in range(7):
-                    final_string += str(k+1) + "  "
-                final_string += "\n"
-            final_string += str(i+1) + "  "
             for j in range(7):
-                final_string += self.__bd[i][j]
-            final_string += "\n"
-        return final_string
+                final_str += str(self.__bd[i][j]) + " "
+            final_str += "\n"
+
+        for i in range(7):
+            final_str += str(i + 1) + "  "
+        final_str += "\n"
+        return final_str
